d278d5eb890ad70343e7486aa6d3e92045c93536
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-11-10 14:43:31 +0500

    add Title component

--- todo/src/components/title/title.js
+++ todo/src/components/title/title.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Title = () => ();
+const Title = ({ title }) => (
+    <h1 className="title">{title}</h1>
+);
+
+Title.propTypes = {
+    title: PropTypes.string,
+};
+
+Title.defaultProps = {
+    title: 'Simple title',
+};
 
 export default Title;
