9fdd5cb5879ee049b31b678dac62c43c03be2206
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-11-10 16:01:56 +0500

    add ToDoInput component and font-awesome

--- todo/src/components/todo-input/todo-input.js
+++ todo/src/components/todo-input/todo-input.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ToDoInput = () => ();
+const ToDoInput = ({ onChange }) => (
+    <div className="todo-input-wrapper">
+        <i className="fas fa-plus" />
+        <input
+            className="todo-input"
+            placeholder="Click to add task"
+            onChange={onChange}
+        />
+    </div>
+);
+
+ToDoInput.propTypes = {
+    onChange: PropTypes.func,
+};
+
+ToDoInput.defaultProps = {
+    onChange: () => {},
+};
 
 export default ToDoInput;
