8923d3af86d68127aa0ba7aa9fb3f15c7d81ada6
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-11-11 15:31:34 +0500

    add css styles

--- todo/src/components/todo-item/todo-item.js
+++ todo/src/components/todo-item/todo-item.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ToDoItem = ({ text, isCompleted }) => {
+const ToDoItem = ({ text, isCompleted }) => (
     <li className="todo-item">
         <i className={isCompleted ? 'mark far fa-check circle' : 'mark far fa-circle'} />
         <span className={isCompleted ? 'completed text' : 'text'}>{text}</span>
         <i className="fas fa-times" />
     </li>
-};
+);
 
 ToDoItem.propTypes = {
     text: PropTypes.string,
