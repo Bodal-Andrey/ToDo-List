f62d14d19cf908f6b9257b532a5405cd16ecf881
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-12-16 16:05:43 +0500

    Вернёт обратно случайно удалённый проект

---
+++ todo/src/actions/actionCreator.js
@@ -0,0 +1,30 @@
+import { ADD_TASK, REMOVE_TASK, COMPLETE_TASK, CHANGE_FILTER } from '../const';
+
+const addTask = (id, text, isCompleted) => ({
+    type: ADD_TASK,
+    id,
+    text,
+    isCompleted
+});
+
+const removeTask = id => ({
+    type: REMOVE_TASK,
+    id
+});
+
+const completeTask = id => ({
+    type: COMPLETE_TASK,
+    id
+});
+
+const changeFilter = activeFilter => ({
+    type: CHANGE_FILTER,
+    activeFilter,
+})
+
+export {
+    addTask,
+    removeTask,
+    completeTask,
+    changeFilter,
+};
--- todo/src/store.js
+++ todo/src/store.js
@@ -6,11 +6,11 @@ import rootReducer from './reducers/index';
 const composeEnhancers =
     process.env.NODE_ENV !== 'production' &&
         typeof window === 'object' &&
-        (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-        (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 /* eslint-enable */
 
-const configureStore = (preloadedState: any) => (
+const configureStore = preloadedState => (
     createStore(
         rootReducer,
         preloadedState,
